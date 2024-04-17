9a4ac3c38865ea49e24aa5882503b64a41a540e0
yusef02 <youssef.bouziane2402@gmail.com>
2024-04-17 16:57:54 +0200

    init vue js project

---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<script>
+import { store } from "store";
+export default {
+	data() {
+		return {
+			store,
+		};
+	},
+};
+</script>
+<template></template>
+<style></style>
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+export const store = reactive([
+	(api = {
+		url: "http://127.0.0.1:8000",
+	}),
+]);
