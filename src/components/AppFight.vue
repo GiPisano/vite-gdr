0021a515d2197e7d5110c88575ec8ae9d9ef0d7d
yusef02 <youssef.bouziane2402@gmail.com>
2024-04-18 14:16:33 +0200

    add logic fight

--- src/components/AppFight.vue
+++ src/components/AppFight.vue
@@ -8,6 +8,14 @@ export default {
 			userCharacter: {},
 			cpuCharacter: {},
 			winMessage: "",
+			userAttack: "",
+			userDefence: "",
+			userLife: "",
+			userCounterAttack: 0,
+			cpuAttack: "",
+			cpuDefence: "",
+			cpuLife: "",
+			cpuCounterAttack: 0,
 		};
 	},
 	methods: {
@@ -21,22 +29,66 @@ export default {
 			});
 		},
 		fight() {
-			let winner = "";
+			this.userLife = this.userCharacter.life;
+			this.cpuLife = this.cpuCharacter.life;
 
-			const dado = store.generateRandNumber(1, 20);
-			const defence = 10;
-			const userStrenght = this.userCharacter.strength;
-			const cpuDefence = this.cpuCharacter.defence;
-			if (userStrenght + dado >= cpuDefence + 10) {
-				winner = "USER";
-			} else {
-				winner = "CPU";
-			}
-			this.winMessage = `Il personaggio ${this.userCharacter.name
-				} scelto dall'utente ha attaco di valore ${userStrenght} + valore del dado ${dado} = ${userStrenght + dado
-				}. \nIl personaggio ${this.cpuCharacter.name
-				} scelto dalla cpu ha difesa di valore ${cpuDefence} + 10 = ${cpuDefence + defence} . \nIl vincitore è ${winner}.`;
-			console.log(winMessage);
+			do {
+				// USER ATTACK
+				let dado = store.generateRandNumber(1, 20);
+				const defence = 10;
+
+				this.userAttack = this.userCharacter.strength + dado;
+				this.cpuDefence = this.cpuCharacter.defence + defence;
+
+				if (this.userAttack > this.cpuDefence) {
+					this.cpuLife = this.cpuLife - (this.userAttack - this.cpuDefence);
+				} else {
+					this.cpuCounterAttack = this.cpuDefence - this.userAttack;
+					this.userLife = this.userLife - this.cpuCounterAttack;
+				}
+				console.log(
+					`LO USER ATTACCA \n \n user attack: ${this.userAttack} \ncpu defence: ${this.cpuDefence} \ncpu conterattack : ${this.cpuCounterAttack} \nuser life: ${this.userLife} \ncpu life: ${this.cpuLife}`
+				);
+
+				// CPU ATTACK
+
+				dado = store.generateRandNumber(1, 20);
+
+				this.cpuAttack = this.cpuCharacter.strength + dado;
+				this.userDefence = this.userCharacter.defence + defence;
+
+				if (this.cpuAttack > this.userDefence) {
+					this.userLife = this.userLife - (this.cpuAttack - this.userDefence);
+				} else {
+					this.cpuCounterAttack = this.userDefence - this.cpuAttack;
+					this.cpuLife = this.cpuLife - this.userCounterAttack;
+				}
+				console.log(
+					`LA CPU ATTACA \n \n cpu attack: ${this.cpuAttack} \nuser defence: ${this.userDefence} \nuser conterattack : ${this.userCounterAttack} \nuser life: ${this.userLife} \ncpu life: ${this.cpuLife}`
+				);
+			} while (this.userLife > 0 && this.cpuLife > 0);
+
+			// let winner = "";
+
+			// const dado = store.generateRandNumber(1, 20);
+			// const defence = 10;
+			// const userStrenght = this.userCharacter.strength;
+			// const cpuDefence = this.cpuCharacter.defence;
+			// if (userStrenght + dado >= cpuDefence + 10) {
+			// 	winner = "USER";
+			// } else {
+			// 	winner = "CPU";
+			// }
+			// this.winMessage = `Il personaggio ${
+			// 	this.userCharacter.name
+			// } scelto dall'utente ha attaco di valore ${userStrenght} + valore del dado ${dado} = ${
+			// 	userStrenght + dado
+			// }. \nIl personaggio ${
+			// 	this.cpuCharacter.name
+			// } scelto dalla cpu ha difesa di valore ${cpuDefence} + 10 = ${
+			// 	cpuDefence + defence
+			// } . \nIl vincitore è ${winner}.`;
+			// console.log(winMessage);
 		},
 	},
 	created() {
@@ -53,16 +105,30 @@ export default {
 				<div class="card">
 					<div class="card-head text-center">
 						<h5 class="card-title">USER</h5>
-						<p class="card-text"><strong>name: </strong>{{ this.userCharacter.name }}</p>
+						<p class="card-text">
+							<strong>name: </strong>{{ this.userCharacter.name }}
+						</p>
 					</div>
 					<div class="list-group list-group-flush">
-						<p class="list-group-item"><strong>strength: </strong>{{ this.userCharacter.strength }}</p>
-						<p class="list-group-item"><strong>defence: </strong>{{ this.userCharacter.defence }}</p>
-						<p class="list-group-item"><strong>intelligence: </strong>{{ this.userCharacter.intelligence }}
+						<p class="list-group-item">
+							<strong>strength: </strong>{{ this.userCharacter.strength }}
 						</p>
-						<p class="list-group-item"><strong>speed: </strong>{{ this.userCharacter.speed }}</p>
-						<p class="list-group-item"><strong>life: </strong>{{ this.userCharacter.life }}</p>
-						<p class="list-group-item"><strong>type: </strong>{{ this.userCharacter.type.name }}</p>
+						<p class="list-group-item">
+							<strong>defence: </strong>{{ this.userCharacter.defence }}
+						</p>
+						<p class="list-group-item">
+							<strong>intelligence: </strong
+							>{{ this.userCharacter.intelligence }}
+						</p>
+						<p class="list-group-item">
+							<strong>speed: </strong>{{ this.userCharacter.speed }}
+						</p>
+						<p class="list-group-item">
+							<strong>life: </strong>{{ this.userCharacter.life }}
+						</p>
+						<p class="list-group-item">
+							<strong>type: </strong>{{ this.userCharacter.type.name }}
+						</p>
 					</div>
 				</div>
 			</div>
@@ -71,16 +137,30 @@ export default {
 				<div class="card">
 					<div class="card-head text-center">
 						<h5 class="card-title">CPU</h5>
-						<p class="card-text"><strong>name: </strong>{{ this.cpuCharacter.name }}</p>
+						<p class="card-text">
+							<strong>name: </strong>{{ this.cpuCharacter.name }}
+						</p>
 					</div>
 					<div class="list-group list-group-flush">
-						<p class="list-group-item"><strong>strength: </strong>{{ this.cpuCharacter.strength }}</p>
-						<p class="list-group-item"><strong>defence: </strong>{{ this.cpuCharacter.defence }}</p>
-						<p class="list-group-item"><strong>intelligence: </strong>{{ this.cpuCharacter.intelligence }}
+						<p class="list-group-item">
+							<strong>strength: </strong>{{ this.cpuCharacter.strength }}
 						</p>
-						<p class="list-group-item"><strong>speed: </strong>{{ this.cpuCharacter.speed }}</p>
-						<p class="list-group-item"><strong>life: </strong>{{ this.cpuCharacter.life }}</p>
-						<p class="list-group-item"><strong>type: </strong>{{ this.cpuCharacter.type.name }}</p>
+						<p class="list-group-item">
+							<strong>defence: </strong>{{ this.cpuCharacter.defence }}
+						</p>
+						<p class="list-group-item">
+							<strong>intelligence: </strong
+							>{{ this.cpuCharacter.intelligence }}
+						</p>
+						<p class="list-group-item">
+							<strong>speed: </strong>{{ this.cpuCharacter.speed }}
+						</p>
+						<p class="list-group-item">
+							<strong>life: </strong>{{ this.cpuCharacter.life }}
+						</p>
+						<p class="list-group-item">
+							<strong>type: </strong>{{ this.cpuCharacter.type.name }}
+						</p>
 					</div>
 				</div>
 			</div>
